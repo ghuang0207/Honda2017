e76a91045bd863a8c8f46ea8503b31181bbc5091
dawnwang15 <dawnwang@bu.edu>
2016-11-29 23:26:28 +0800

    Complete 3rd tab - get topics by subject and by state

--- azHonda/js/controllers/summary.js
+++ azHonda/js/controllers/summary.js
@@ -1,12 +1,11 @@
 ﻿'use strict';
 var app = angular.module("hondaApp");
 
-app.controller("SummaryCtrl", function ($scope, $mdDialog, $sce, SrvData) {
+app.controller("SummaryCtrl", function ($scope, $mdDialog, $sce, SrvData, $filter) {
     $scope.states = [];
     $scope.categories = [];
     $scope.subjects = [];
     $scope.allTopics = [];
-    $scope.list_subject_topics = [];
 
     SrvData.ListAllCategories().then(function (response) {
         $scope.categories = response.data;
@@ -37,20 +36,56 @@ app.controller("SummaryCtrl", function ($scope, $mdDialog, $sce, SrvData) {
     $scope.ListAllSubjects();
     $scope.ListAllTopics();
 
-    $scope.showDialog = function (ev, StateInfo, categoryId) {
+    $scope.ShowStateTopicsDialog = function (ev, StateInfo, categoryId) {
+        SrvData.GetTopics_by_State(StateInfo.StateCode, categoryId).then(function (response) {
+            var stateTopics = []
+            if (response.data != "null") {
+                stateTopics = response.data;
+            }
+            //Dialog Control Init
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'views/dialogs/topics-modal.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: false,
+                fullscreen: $scope.customFullscreen, // Only for -xs, -sm breakpoints.
+                locals: {
+                    DisplayTopics: stateTopics,
+                    Info: {
+                        StateInfo: StateInfo.StateName,
+                        categoryId: categoryId
+                    }
+                }
+            })
+            .then(function (returnVal) {
+                $scope.ListAllTopics();
+                $scope.ListAllSubjects();
+            }, function () {
+                $scope.ListAllTopics();
+                $scope.ListAllSubjects();
+            });
+
+        }, function (err) {
+            console.log(err);
+        });
+    };
+
+    $scope.ShowSubjectTopicsDialog = function (ev, AllTopics, subject, stateCode) {
+        //Dialog Control Init
+        var SubjectTopics = $filter('filter')(AllTopics, subject, true, 'Subject');
+        SubjectTopics = $filter('unique')(SubjectTopics, 'State');
         $mdDialog.show({
             controller: DialogController,
             templateUrl: 'views/dialogs/topics-modal.html',
             parent: angular.element(document.body),
             targetEvent: ev,
-            clickOutsideToClose: true,
+            clickOutsideToClose: false,
             fullscreen: $scope.customFullscreen, // Only for -xs, -sm breakpoints.
-            resolve: {
-                StateInfo: function () {
-                    return StateInfo
-                },
-                categoryId: function () {
-                    return categoryId
+            locals: {
+                DisplayTopics: SubjectTopics,
+                Info: {
+                    subject: subject
                 }
             }
         })
@@ -61,81 +96,69 @@ app.controller("SummaryCtrl", function ($scope, $mdDialog, $sce, SrvData) {
             $scope.ListAllTopics();
             $scope.ListAllSubjects();
         });
+    };
 
-        // this is for the popup
-        function DialogController($scope, $mdDialog, StateInfo, categoryId) {
-            $scope.options = {
-                height: 150,
-                toolbar: [
-                  ['style', ['bold', 'italic', 'underline', 'clear']],
-                  ['color', ['color']],
-                  ['para', ['ul', 'ol', 'paragraph']],
-                  ['height', ['height']]
-                ]
-            };
-
-            $scope.StateInfo = StateInfo;
-            $scope.categoryId = categoryId;
-            $scope.stateTopics = [];
-            $scope.getStateTopics = function () {
-                SrvData.GetTopics_by_State($scope.StateInfo.StateCode, $scope.categoryId).then(function (response) {
-                    if (response.data != "null") {
-                        $scope.stateTopics = response.data;
-                    }
-                    else {
-                        $scope.stateTopics = [];
-                    }
+    // Dialog scope directive
+    function DialogController($scope, $mdDialog, DisplayTopics, Info) {
+        $scope.Topics = DisplayTopics;
+        if ("categoryId" in Info) {
+            $scope.isAllowEdit = true;
+            $scope.StateInfo = Info.StateInfo;
+            $scope.categoryId = Info.categoryId;
+            $scope.title = $scope.StateInfo.StateName;
+        }
+        if ("subject" in Info) {
+            $scope.isAllowEdit = false;
+            $scope.subject = Info.subject
+            $scope.title = $scope.subject;
+        }
+        
+        $scope.AddNewTopic = function () {
+            var newTopic = { 'TopicId': '', 'State': { 'StateCode': $scope.StateInfo.StateCode }, 'Category': { 'CategoryId': $scope.categoryId } };
+            $scope.stateTopics.push(newTopic);
+            console.log(newTopic);
+        };
+        $scope.EditTopic = function (changedTopic) {
+            console.log(changedTopic)
+            changedTopic.isEdit = true;
+        };
+        $scope.DeleteTopic = function (changedTopic) {
+            if (confirm('Are you sure to delete the topic?')) {
+                SrvData.DeleteTopic_by_TopicId(changedTopic.TopicId).then(function (response) {
+                    var index = $scope.stateTopics.indexOf(changedTopic);
+                    $scope.stateTopics.splice(index, 1);
                 }, function (err) {
                     console.log(err);
                 });
             }
-            $scope.getStateTopics();
-            $scope.cancel = function () {
-                $mdDialog.cancel();
-            };
-            $scope.print = function () {
-                alert("Print this document.")
-                //$mdDialog.hide(returnVal);
-            };
-            $scope.AddNewTopic = function () {
-                var newTopic = { 'TopicId': '', 'State': { 'StateCode': $scope.StateInfo.StateCode }, 'Category': { 'CategoryId': $scope.categoryId } };
-                $scope.stateTopics.push(newTopic);
-                console.log(newTopic);
-            };
-            $scope.EditTopic = function (changedTopic) {
-                console.log(changedTopic)
-                changedTopic.isEdit = true;
-            };
-            $scope.DeleteTopic = function (changedTopic) {
-                if (confirm('Are you sure to delete the topic?')) {
-                    SrvData.DeleteTopic_by_TopicId(changedTopic.TopicId).then(function (response) {
-                        var index = $scope.stateTopics.indexOf(changedTopic);
-                        $scope.stateTopics.splice(index, 1);
-                    }, function (err) {
-                        console.log(err);
-                    });
-                }
-            };
-            $scope.CancelChange = function (changedTopic) {
-                debugger;
-                if (changedTopic.TopicId == '') {
-                    var index = $scope.stateTopics.indexOf(changedTopic);
-                    $scope.stateTopics.splice(index, 1);
-                }
-                else {
-                    changedTopic.isEdit = false;
-                }
-            };
-            $scope.SaveChange = function (changedTopic) {
-                SrvData.addUpdateTopic(changedTopic.TopicId, changedTopic.Subject, changedTopic.Content, changedTopic.State.StateCode, changedTopic.Category.CategoryId).then(function (response) {
-                    //updatedTopicId = response.data
-                    $scope.getStateTopics();
-                }, function (err) {
-                    console.log(err);
-                });
-            };
-        }
+        };
+        $scope.CancelChange = function (changedTopic) {
+            debugger;
+            if (changedTopic.TopicId == '') {
+                var index = $scope.stateTopics.indexOf(changedTopic);
+                $scope.stateTopics.splice(index, 1);
+            }
+            else {
+                changedTopic.isEdit = false;
+            }
+        };
+        $scope.SaveChange = function (changedTopic) {
+            SrvData.addUpdateTopic(changedTopic.TopicId, changedTopic.Subject, changedTopic.Content, changedTopic.State.StateCode, changedTopic.Category.CategoryId).then(function (response) {
+                //updatedTopicId = response.data
+                $scope.getStateTopics();
+            }, function (err) {
+                console.log(err);
+            });
+        };
 
-    }
+        //Dialog controls
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+        $scope.print = function () {
+            alert("Print this document.")
+            //$mdDialog.hide(returnVal);
+        };
+    };
 
 });
